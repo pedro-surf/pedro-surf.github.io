4877191c3256bdfc6c2f0b3ca5330e6bdbcb4a98
pedro-surf <pedroaki.55@gmail.com>
2025-10-21 17:39:20 -0300

    fix build manually

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c053b2dfc33cf970b47a906ddd8a857.js"
+  "/precache-manifest.7322ff2db0a4e99e3bda1de4e6f564d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
