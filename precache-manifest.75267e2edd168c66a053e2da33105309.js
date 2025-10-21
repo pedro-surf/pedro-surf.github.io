b4ada00557bfd5bf294f1719140ed12a07d13c0f
pedro-surf <pedroaki.55@gmail.com>
2025-10-21 20:08:07 -0300

    Updates

---
+++ precache-manifest.75267e2edd168c66a053e2da33105309.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cd7211808bc77182153d2687580f51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f812c88f641d8fbc1695",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "f998532ced618f34f72c",
+    "url": "/static/css/main.c616dae5.chunk.css"
+  },
+  {
+    "revision": "f812c88f641d8fbc1695",
+    "url": "/static/js/2.e4b985c5.chunk.js"
+  },
+  {
+    "revision": "f998532ced618f34f72c",
+    "url": "/static/js/main.64c4b566.chunk.js"
+  },
+  {
+    "revision": "416cc1c79b8c22940a8c",
+    "url": "/static/js/runtime~main.7302273d.js"
+  },
+  {
+    "revision": "20c18bcbd13ff265b192b5353b954d0a",
+    "url": "/static/media/coding.20c18bcb.jpg"
+  },
+  {
+    "revision": "5610fbd220f7018220f764e5e19fc215",
+    "url": "/static/media/motherforest.5610fbd2.jpg"
+  }
+]);
